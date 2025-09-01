f34b62b85c9aede63f3f4b5dea492a842ee2a49a
momen-tarek111 <momen.tarek.nagaty@gmail.com>
2025-09-01 03:05:43 +0300

    second upload

--- src/utils/request.js
+++ src/utils/request.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 
 const request=axios.create({
-    baseURL:"apiblogapp-production.up.railway.app/"
+    baseURL:"apiblogapp-production.up.railway.app"
 })
 
 export default request;
